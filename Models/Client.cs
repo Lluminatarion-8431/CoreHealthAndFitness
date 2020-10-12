db34824ce48c452eba012dc409762f49767044b3
Lluminatarion-8431 <Captain.a.8431@gmail.com>
2020-10-12 12:33:54 -0700

    Core Health And Fitness init.

---
+++ Models/Client.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Health_and_Fitness.Models
+{
+    public class Client
+    {
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Core_Health_and_Fitness.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
